1c64eeb9615f88e81d74e3da7cb8ef56b690b23a
qorlgns1 <rlgns0610@gmail.com>
2021-10-12 00:11:09 +0900

    webpack Plugins 추가
    HtmlWebpackPlugin, CleanWebpackPlugin 사용
    
    new CleanWebpackPlugin(['dist']) 이부분에서 에러가 발생했다.
    CleanWebpackPlugin 깃허브에 들어가서 사용방법을 보니 dist 를 없앤 new CleanWebpackPlugin() 으로 실행하니 정상적으로 동작되는것을 확인했다..
    위 설명처럼 index.html 파일이 생성되었고, 이때 생성된 index.html에서는 번들 파일 main.js를 로드했다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,4 +16,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
 };
